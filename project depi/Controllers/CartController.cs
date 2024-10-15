da6f17afa16b45ec7097e8516ec2f62bf8bad28e
Ahmed Mohmed <111065068+Ahmed7773333@users.noreply.github.com>
2024-10-15 13:20:31 +0200

    Controllers init

---
+++ project depi/Controllers/CartController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using project_depi.Data_Layer;
+using project_depi.Data_Layer.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace project_depi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CartController : ControllerBase
+    {
+        private readonly AppContextDB _context;
+
+        public CartController(AppContextDB context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cart
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cart>>> GetCarts()
+        {
+            return await _context.Carts.ToListAsync();
+        }
+
+        // GET: api/Cart/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cart>> GetCart(Guid id)
+        {
+            var cart = await _context.Carts.FindAsync(id);
+
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return cart;
+        }
+
+        // POST: api/Cart
+        [HttpPost]
+        public async Task<ActionResult<Cart>> PostCart(Cart cart)
+        {
+            _context.Carts.Add(cart);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCart), new { id = cart._id }, cart);
+        }
+
+        // PUT: api/Cart/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCart(Guid id, Cart cart)
+        {
+            if (id != cart._id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cart).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CartExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Cart/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCart(Guid id)
+        {
+            var cart = await _context.Carts.FindAsync(id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            _context.Carts.Remove(cart);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CartExists(Guid id)
+        {
+            return _context.Carts.Any(e => e._id == id);
+        }
+    }
+}
---
+++ project depi/Controllers/CategoryController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using project_depi.Data_Layer;
+using project_depi.Data_Layer.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace project_depi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly AppContextDB _context;
+
+        public CategoryController(AppContextDB context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Category
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        // GET: api/Category/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategory(Guid id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+
+        // POST: api/Category
+        [HttpPost]
+        public async Task<ActionResult<Category>> PostCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCategory), new { id = category._id }, category);
+        }
+
+        // PUT: api/Category/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(Guid id, Category category)
+        {
+            if (id != category._id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Category/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(Guid id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CategoryExists(Guid id)
+        {
+            return _context.Categories.Any(e => e._id == id);
+        }
+    }
+}
